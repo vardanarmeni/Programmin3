78ce0f100ccbc8ad498c207e76a5a4f85aac2339
ArmLil <armlilhov@mail.ru>
2018-04-25 22:46:15 +0400

    node-modules

--- public/script.js
+++ public/script.js
@@ -1,18 +1,8 @@
-var n = 50, m = 50;
-var side = 15;
-var matrix = [];
-for (var y = 0; y < m; ++y) {
-  matrix.push([]);
-  for (var x = 0; x < n; x += 1) {
-    matrix[y].push(Math.round(Math.random() * 4.6))
-  }
-}
+function nerkel()
+{
 
-var grassArr = [];
-var xotakerArr = [];
-var gishatichArr = [];
-var ararichArr = [];
-var vorsordArr = [];
+fill();
+}
 
 function setup() {
   frameRate(1);
@@ -45,35 +35,7 @@ function setup() {
   }
 }
 
-function draw() {
-  for (var y = 0; y < matrix.length; y++) {
-    for (var x = 0; x < matrix[y].length; x++) {
-      if (matrix[y][x] == 1) {
-        fill("green");
-        rect(x * side, y * side, side, side);
-      }
-      else if (matrix[y][x] == 0) {
-        fill("#E5E0DB");
-        rect(x * side, y * side, side, side);
-      }
-      else if (matrix[y][x] == 2) {
-        fill("yellow");
-        rect(x * side, y * side, side, side);
-      }
-      else if (matrix[y][x] == 3) {
-        fill("brown");
-        rect(x * side, y * side, side, side);
-      }
-      else if (matrix[y][x] == 4) {
-        fill('#DFAB70');
-        rect(x * side, y * side, side, side);
-      }
-      else if (matrix[y][x] == 5) {
-        fill("blue");
-        rect(x * side, y * side, side, side);
-      }
-    }
-  }
+
 
   for (var i in grassArr) {
     grassArr[i].bazmanal();
@@ -90,4 +52,3 @@ function draw() {
   for (var i in vorsordArr) {
     vorsordArr[i].spanel();
   }
-}
